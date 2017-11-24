eb73534b51315c7e6e861d9a1ebcac68a730f205
Raghu Raman <rraagghhu@gmail.com>
2017-11-24 14:57:20 +0530

    Updating target.

--- target/mark-6/pwa-sw-GAMMA_ALL_LANGUAGE_GR.js
+++ target/mark-6/pwa-sw-GAMMA_ALL_LANGUAGE_GR.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?201120171710',
+		'pwa-stylesheets/css/style.css?241120171456',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -27,7 +27,7 @@
 		'https://www.ptlp.co/resource-all/font/font-en.css'
 	];
 
-	var STATIC_VERSION = "201120171710";
+	var STATIC_VERSION = "241120171456";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -36,7 +36,7 @@
 	var apiPrefix = "https://www-gamma-gr.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-201120171710.html";
+	var PWA_INDEX_HTML = "app-shell-241120171456.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
